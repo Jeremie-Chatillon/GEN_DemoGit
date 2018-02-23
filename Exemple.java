24a7832007cb42e2b7148837753cb6da15aaa869
James <james.smith@heig-vd.ch>
2018-02-23 10:41:27 +0100

    Salut

--- Exemple.java
+++ Exemple.java
@@ -1,9 +1,9 @@
 // Test confilct merge
 
-// Ceci est le code de jérémie
+// Ceci est le code de james
 public class MatrixXorOperator extends MatrixOperator {
 
     public boolean operate(final boolean x, final boolean y) {
-        return x ^ y;
+        return x * y;
     }
 }
