cdd863a8fd7e93f49e3f41e971838991032ac3ba
Unknown <jeremie.chatillon@heig-vd.ch>
2018-02-23 10:43:41 +0100

    Auto stash before merge of "master" and "Jeremie"

--- Exemple.java
+++ Exemple.java
@@ -1,17 +1,10 @@
 // Test confilct merge
 
 // Ceci est le code de jérémie
-<<<<<<< Updated upstream
-public class MatrixXorOperator extends MatrixOperator {
-
-    public boolean operate(final boolean x, final boolean y) {
-        return x ^ y;
-=======
 // Modifié par Jérémie
 public class MatrixXorOperator extends MatrixOperator {
 
     public boolean operate(final boolean x, final boolean y) {
         return x / y;
->>>>>>> Stashed changes
     }
 }
