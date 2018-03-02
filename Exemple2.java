48ee9c571830c1b03096e6db66a2fd39f7939dac
Unknown <jeremie.chatillon@heig-vd.ch>
2018-03-02 11:01:48 +0100

    Test

---
+++ Exemple2.java
@@ -0,0 +1,10 @@
+// Test confilct merge
+
+// Ceci est le code de jérémie
+public class MatrixXorOperator extends MatrixOperator {
+
+// it's false
+    public boolean operate(final boolean x, final boolean y) {
+        return x * y;
+    }
+}
